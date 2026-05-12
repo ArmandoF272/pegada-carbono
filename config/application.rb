cccc928c806935823ffa385d1960d98b6450b03e
Armando Vieira <programador12@speedgov.com.br>
2026-05-12 19:29:45 -0300

    feat: adiciona locale pt-BR e tradução completa do Devise

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module PegadaCarbono
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.available_locales = [:"pt-BR", :en]
   end
 end
